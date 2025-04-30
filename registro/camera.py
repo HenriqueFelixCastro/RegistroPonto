f010762948880c20005cff0a46430cd15676b370
Seu Nome <henrique.felix.castro@hotmail.com>
2025-04-29 22:02:47 -0300

    Extração Facial

--- registro/camera.py
+++ registro/camera.py
@@ -9,6 +9,10 @@ class VideoCamera(object):
         if not self.video.isOpened():
             print("Erro ao acessar a câmera.")
 
+        self.img_dir = "./tmp"
+        if not os.path.exists(self.img_dir):
+            os.makedirs(self.img_dir)  # Cria a pasta `tmp` se não existir
+
     def __del__(self):
         self.video.release()
 
@@ -26,10 +30,8 @@ class VideoCamera(object):
     def detect_face(self):
         ret, frame = self.get_camera()
         if not ret or frame is None:
-            print("Falha ao capturar o frame")
+            print("Falha ao capturar o frame.")
             return None
-
-        # Defina a região de interesse (ROI) onde o rosto será detectado
 
         altura, largura, _ = frame.shape
         centro_x, centro_y = int(largura / 2), int(altura / 2)
@@ -37,15 +39,35 @@ class VideoCamera(object):
         x1, y1 = centro_x - a, centro_y - b
         x2, y2 = centro_x + a, centro_y + b
         roi = frame[y1:y2, x1:x2]
-        # Converta a ROI em escala de cinza para a detecção de faces
+
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-        # Define cor do círculo: vermelho (sem rosto) ou verde (com rosto)
         ellipse_color = (0, 255, 0) if len(faces) > 0 else (0, 0, 255)
-
-        # Desenha o círculo principal (verde ou vermelho)
         cv2.ellipse(frame, (centro_x, centro_y), (a, b), 0, 0, 360, ellipse_color, 2)
 
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
+
+    def sample_faces(self, frame):
+        ret, frame = self.get_camera()
+        if not ret or frame is None:
+            return None
+
+        frame = cv2.flip(frame, 180)
+        frame = cv2.resize(frame, (480, 360))
+
+        faces = self.face_cascade.detectMultiScale(
+            frame,
+            scaleFactor=1.1,
+            minNeighbors=20,
+            minSize=(30, 30),
+            maxSize=(400, 400)
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+            cropped_face = frame[y:y+h, x:x+w]
+            return cropped_face
+
+        return None
--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,5 @@
+import cv2
+import os
 from django.shortcuts import render, redirect
 from .forms import FuncionarioForm, ColetaFacesForm
 from .models import Funcionario, ColetaFaces
@@ -31,14 +33,71 @@ def criar_funcionario(request):
         form = FuncionarioForm()
     return render(request, 'criar_funcionario.html', {'form': form})
 
+def extract(camera_detection, funcionario_slug):
+    amostra = 0  # Amostras inicial
+    numeroAmostras = 10  # Número de amostras para extrair
+    largura, altura = 220, 220  # Tamanho da imagem
+    file_paths = []  # Lista de caminhos das amostras
+
+    while amostra < numeroAmostras:
+        ret, frame = camera_detection.get_camera()  # Pega frame da câmera
+        crop = camera_detection.sample_faces(frame)  # Captura a face
+
+        if crop is not None:
+            amostra += 1
+            face = cv2.resize(crop, (largura, altura))  # Redimensiona
+            imagemCinza = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)  # Converte para cinza
+            file_name_path = f'./tmp/{funcionario_slug}_{amostra}.jpg'
+            cv2.imwrite(file_name_path, imagemCinza)
+            file_paths.append(file_name_path)
+        else:
+            print("Face não encontrada")
+
+        if amostra >= numeroAmostras:
+            break
+
+    camera_detection.restart()  # Reinicia a câmera após as capturas
+    return file_paths
+
+
+def face_extract(context, funcionario):
+    num_coletas = ColetaFaces.objects.filter(
+        funcionario__slug=funcionario.slug
+    ).count()
+
+    if num_coletas >= 10:
+        context['erro'] = 'Limite máximo de coletas atingido.'
+    else:
+        files_paths = extract(camera_detection, funcionario.slug)  # Captura faces e retorna paths
+        for path in files_paths:
+            # Cria uma instância de ColetaFaces e salva a imagem
+            coleta_face = ColetaFaces.objects.create(funcionario=funcionario)
+            coleta_face.image.save(os.path.basename(path), open(path, 'rb'))
+            os.remove(path)  # Remove o arquivo temporário após o salvamento
+
+        # Atualiza o contexto com as coletas salvas
+        context['file_paths'] = ColetaFaces.objects.filter(
+            funcionario__slug=funcionario.slug
+        )
+        context['extracao_ok'] = True  # Define sinalizador de sucesso
+
+    return context
+
 # Cria coleta de faces (Registro)
 def criar_coleta_faces(request, funcionario_id):
-    print(funcionario_id)  # Identificador do funcionário cadastrado
-    funcionario = Funcionario.objects.get(id=funcionario_id)  # Resgata o funcionário
-    
+    funcionario = Funcionario.objects.get(id=funcionario_id)
+    botao_clicado = request.GET.get('clicked', 'False') == 'True'
+
     context = {
-        'funcionario': funcionario,  # Passa o objeto funcionario para o template
-        'face_detection': face_detection,  # Passa camera aqui para renderizar no template
+        'funcionario': funcionario,
+        'face_detection': face_detection,
+        'valor_botao': botao_clicado,
     }
-    
+
+    if botao_clicado:
+        print("Cliquei em Extrair Imagens")
+        # Chama a função de extração de imagens faciais
+        context = face_extract(context, funcionario)
+
     return render(request, 'criar_coleta_faces.html', context)
+
